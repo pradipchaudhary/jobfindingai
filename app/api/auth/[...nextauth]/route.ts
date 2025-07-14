530fe5a21eb11e13005f4b58cb705d61b8ec774f
Pradip Chaudhary <chaudharypradip678@gmail.com>
2025-07-14 23:55:35 +0545

    register api issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,7 @@
-import NextAuth from "next-auth"
-import GitHubProvider from "next-auth/providers/github"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/lib/prisma"
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-})
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
 
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
